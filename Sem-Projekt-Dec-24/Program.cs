868899f708994b56d1869a913f14eadef5cbf801
clausz <clauszxc@users.noreply.github.com>
2024-11-25 13:42:44 +0100

    Added MainForm and deleted Form1

--- Sem-Projekt-Dec-24/Program.cs
+++ Sem-Projekt-Dec-24/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Sem_Projekt_Dec_24.Winforms;
 
 namespace Sem_Projekt_Dec_24
 {
@@ -16,7 +17,7 @@ namespace Sem_Projekt_Dec_24
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
