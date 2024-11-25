5d3f701def9d186d26905d9983d7e4dcd5635a5d
Anders Bøgehøj Iversen <andersbiversen35@gmail.com>
2024-11-25 14:17:06 +0100

    Fuckup

--- Sem-Projekt-Dec-24/Program.cs
+++ Sem-Projekt-Dec-24/Program.cs
@@ -19,6 +19,7 @@ namespace Sem_Projekt_Dec_24
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+            Console.WriteLine("Hej");
         }
     }
 }
